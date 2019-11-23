6ea14c34162967d99b706367ec72c9f1a19be336
Daniel Anechitoaie <daniel.anechitoaie@gmail.com>
2019-11-24 01:56:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Code Review Assistant - Sonar Linter
+
+```yaml
+    - name: SonarLinter
+      uses: OSFGlobal/CodeReviewAssistant-SonarLinter@master
+      with:
+          azureAccount: ${{ secrets.AZURE_ACCOUNT }}
+          azureKey: ${{ secrets.AZURE_KEY }}
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const io = require("@actions/io");
+const uuidV4 = require("uuid/v4");
+const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
+
+async function run() {
+    try {
+        const azureAccount = core.getInput("azureAccount", { required: true });
+        const azureKey = core.getInput("azureKey", { required: true });
+
+        const tempDirectory = process.env.RUNNER_TEMP;
+        if (!tempDirectory) {
+            throw new Error("Missing env.RUNNER_TEMP!");
+        }
+
+        const destDirectory = path.join(tempDirectory, uuidV4());
+        await io.mkdirP(destDirectory);
+
+        const jarVer = "1.0.0";
+        const blobURL = `https://${azureAccount}.blob.core.windows.net`;
+
+        const azureCredentials = new StorageSharedKeyCredential(azureAccount, azureKey);
+        const blobServiceClient = new BlobServiceClient(blobURL, azureCredentials);
+        const containerClient = blobServiceClient.getContainerClient("jars");
+
+        const blobName = `sonar-linter-${jarVer}.jar`;
+        const blobClient = containerClient.getBlobClient(blobName);
+
+        const jarPath = path.join(destDirectory, blobName);
+        await blobClient.downloadToFile(jarPath);
+
+        const javaPath = await io.which("java", true);
+        core.setSecret(`${javaPath} -jar ${jarPath}`);
+        await exec.exec(javaPath, [`-jar`, jarPath, `--annotations-type=GITHUB_ACTIONS`]);
+    } catch (e) {
+        core.setFailed(e.message);
+    }
+}
+
+run();
